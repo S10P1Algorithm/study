30455d3d345b34a0bed59c404b62d43e73725f22
sangbumlikeagod <oironese@naver.com>
2024-01-16 09:05:01 +0900

    sangbum comment 7569, 1937, 1107

--- week1/1937/1937_김상범.py
+++ week1/1937/1937_김상범.py
@@ -1,26 +1,69 @@
 import sys
 sys.setrecursionlimit(200000)
+
+
+'''
+# 입력
+
+n * n 의 대나무 숲이 있고 
+
+# 과정
+
+다먹으면 이동하는 룰,다음에 이동하는 지역은 반드시 지금 지역보다 대나무가 많다
+
+# 출력
+
+판다가 이동할 수 있는 칸의 최대값
+
+### 그냥 전부 계산
+
+---
+
+각 행마다 dfs해서 25000 * 25000 ⇒ 최대 625억
+
+그런데 각 dfs마다  어떤 한 칸에서 이동할 수있는 최대의 방향수가 정해졌다면 그건 이전 결과 값에 상관 없이 같다 
+
+4개이며 길이 탐색의 가장 긴 경우의 수는 25000개
+
+이미 한번 갈 수 있는 최대 길이가 정해진 곳은 다시 참조했을때 이전 상황과 상관없이 같은 결과를 낸다 ⇒ 저장해도 문제가 없다 .
+
+dfs의 총 횟수는 25000개로 정해져 있고 한 dfs가 길 수록 이후 dfs의 횟수가 적어진다
+
+(25000 - x)(x) = 1억 살짝넘음
+
+
+'''
+
 N = int(input())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 visited = [[0] * N for _ in range(N)]
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+# dx, dy, dp 설정
 
-
+# dp 함수
 def dfs(x, y):
+    # 방문한적있나?
     if visited[x][y]:
+        # yes일시 저장 값리턴
         return visited[x][y]
-
+    # no일경우 최소 결과값 설정
     ret = 1
+    # 4방향 이동
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
+        # 범위안에 있고 그쪽이 더 큰가?
         if 0 <= nx < N and 0 <= ny < N and matrix[nx][ny] > matrix[x][y]:
+            # yes 일시 저장 돼 있는가? 
             if visited[nx][ny]:
+                # yes 일시 리턴값은 기존의 max값과 저장된 값 + 1
                 ret = max(ret, visited[nx][ny] + 1)
             else:
+                # no 일시 리턴값은 기존의 max값과 새 좌표의 dp 값 + 1
                 ret = max(ret, dfs(nx, ny) + 1)
 
     visited[x][y] = ret
+    # 값 저장
     return ret
 
 
--- week1/7569/7569_김상범.py
+++ week1/7569/7569_김상범.py
@@ -6,28 +6,43 @@ dx = [0, 0, 1, -1, 0, 0]
 dy = [1, -1, 0, 0, 0, 0]
 dz = [0, 0, 0, 0, 1, -1]
 
-
+'''
+3차원 M, N, H크기의 토마토가 있음 안익은 토마토가 있을 수 있어서 안익은 토마토들은 인접한 토마토의 영향을 받아서 익게된다. 
+'''
+# y, x, z 값 받기
 N, M, H = map(int, input().split())
 
+# x, y, z값 만큼의 배열 생성
 box = [[[0] * H for _ in range(N)] for _ in range(M)]
+
+# 익은 과일 큐 설정
 q = deque()
 ans = 0
 remains = 0
 
+# 큐브 값 전처리 
 for z in range(H):
     for x in range(M):
         row = list(map(int, input().split()))
+        # 한 행을 받아옴 
         for y, tmp in enumerate(row):
+            # 행의 열번호, 값을 분해
+            # 값이 1일경우 익은 과일 큐에 추가
             if tmp == 1:
                 q.append((x, y, z))
+            # 값이 0일 경우 익어야 하는 개수에 추가
             elif tmp == 0:
                 remains += 1
             box[x][y][z] = tmp
 
 while True:
     nq = deque()
+    # 섞이지 않기위해 새로운 큐 사용
+    
+    # bfs
     while q:
         x, y, z = q.popleft()
+        # 6방향인게 특징
         for i in range(6):
             nx, ny, nz = x + dx[i], y + dy[i], z + dz[i]
             if (0 <= nx < M) and (0 <= ny < N) and (0 <= nz < H) and box[nx][ny][nz] == 0:
@@ -35,14 +50,19 @@ while True:
                 nq.append((nx, ny, nz))
                 box[nx][ny][nz] = 1
                 # print(nx, ny, nz, end='\t')
-    # print()
 
+    # 익은 토마토가 전부 영향을 끼쳤을때 
+    # 새롭게 익은 큐가 있는가?
     if not nq:
+        # yes일시 중지
         break
     else:
+        # no일시 큐는 nq
         q = nq
+        # 시간 + 1
         ans += 1
-
+        
+# 종료후 익어야할 과일이 남아있는가?
 if remains:
     print(-1)
 else:
