fa1e71c5da7310b283ab5ed446c9898c4ccfce29
Kdh <dohoon122@gmail.com>
2024-01-25 18:08:02 +0900

    dohoon fail

---
+++ week2/16566/16566_김도훈.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+N, M, K = list(map(int, input().split()))
+card = list(map(int, input().split()))
+charles = list(map(int, input().split()))
---
+++ week2/18185/18185_김도훈.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+N = int(input())
+A = list(map(int, input().split()))
+
+arr = [[0, 0, 0] for _ in range(N)]
+arr[0] = [A[0], 0, 0]
+arr[1] = [A[0]+A[1]-min(A[0], A[1]), min(A[0], A[1]), 0]
+
+
+for i in range(2, N):
+    new = A[i]
+    add_5 = min(new,  arr[i-1][0])
+    new -= add_5
+    arr[i-1][0] -= add_5
+    arr[i-1][1] = add_5
+    if new:
+        add_7 = min(new, arr[i-2][1])
+        new -= add_7
+        arr[i-2][1] -= add_7
+        arr[i-2][2] = add_7
+    if new:
+        add_3 = new
+        arr[i][0] = add_3
+
+    
+    print(A)
+
+q = min(A[N-2], A[N-1])
+p = A[N-2] + A[N-1] - q
+
+ans += (p*3 + q*5)
+
+print(ans)
