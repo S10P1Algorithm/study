2ed8162b696ae6e3f4b9a09642bde8301e8f1d20
ooonjae <hil00077@naver.com>
2024-02-05 23:18:46 +0900

    9369 oonjae GG

--- week4/9369/9369_이운재.py
+++ week4/9369/9369_이운재.py
@@ -1,1 +1,36 @@
-print('hi')
+T = int(input())
+
+
+test1 = dict()
+
+for tc in range(1,T+1):
+    N = int(input())
+    encryted = []
+
+    for i in range(N):
+        encryted.append(input())
+
+    decoded = input()
+    problem = input()
+
+    # 1.무지성 딕셔너리 생성
+
+    for i in range(N):
+        if len(decoded) != len(encryted[i]):
+            continue
+        else:
+            for j in range(len(encryted[i])):
+                if encryted[i][j] in test1.keys() and decoded[j] != test1[encryted[i][j]]:
+                    del test1[encryted[i][j]]
+                else:
+                    test1[encryted[i][j]] = decoded[j]
+
+    res = ''
+
+    for i in problem:
+        try:
+            res += test1[i]
+        except:
+            res += '?'
+
+    print(res)
