8c42844464e2bfbc662a317ac1a70fe493cca650
Kim Ji Yong <jykim00324@gmail.com>
2024-03-05 10:06:28 +0900

    jy 1700 comment

--- week8/1700/1700_김지용.py
+++ week8/1700/1700_김지용.py
@@ -1,46 +1,58 @@
 import sys
 
-sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
 
 N, K = map(int, input().split())
 order = list(map(int, input().split()))
 
 d = dict()
 
+# 각강의 숫자가 나오는 횟수를 저장
 for i in order:
     d.setdefault(i, 0)
     d[i] += 1
 
+# 전기콘센트와 결괏값
 plug = set()
 total_cnt = 0
 
+# 부여된 순서대로 탐색
+# 세가지 경우의 수
+# 1. 이미 꽂혀 있는 경우
+# 2. 빈 콘센트가 있는 경우
+# 3. 빈 콘센트가 없고 이미 꽂혀 있지 않은 경우
 for h in range(K):
     j = order[h]
-
+    
+    # 1번 경우
     if j in plug:
         d[j] -= 1
+    
+    # 2번 경우
     elif len(plug) < N:
         plug.add(j)
         d[j] -= 1
+    
+    # 3번 경우
+    # 뒤에서 더 이상 안쓰는 것이 있으면 그걸 뽑음
+    # 모두 다 뒤에 사용하면 가장 나중에 사용하는 걸 뽑음
     else:
-        min_cnt = K
-        curr_min = 0
+        visited = set()
+        for h1 in range(h+1, K):
+            curr_next = order[h1]
+            if curr_next in plug and curr_next not in visited:
+                curr_min = curr_next
+                visited.add(curr_next)
 
         for l in plug:
-            if d[l] < min_cnt:
+            if d[l] == 0:
                 curr_min = l
-                min_cnt = d[l]
-            elif d[l] == min_cnt:
-                for h1 in range(h+1, K):
-                    if order[h1] == curr_min:
-                        curr_min = l
-                        break
+
         plug.remove(curr_min)
-        print('removed', curr_min)
+
         plug.add(j)
         d[j] -= 1
 
         total_cnt += 1
-    print(j, plug, d, total_cnt)
 
 print(total_cnt)
