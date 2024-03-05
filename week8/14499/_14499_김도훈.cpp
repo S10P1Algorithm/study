fa0b652f8b1f14059d707362c06ec4be43ac210f
Kdh <dohoon122@gmail.com>
2024-03-05 17:55:32 +0900

    14499 고민한 흔적

---
+++ week8/14499/_14499_김도훈.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	freopen("input.txt", "r", stdin);
+
+	int N, M, x, y, K;
+	cin >> N >> M >> x >> y >> K;
+	for (int i = 0; i < N; i++) {
+
+	}
+	int* order = new int[K];
+	for (int i = 0; i < K; i++) {
+		cin >> order[i];
+	}
+
+
+
+
+	delete[] order;
+	return 0;
+}
+
