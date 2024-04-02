1b06b6e8d3c1b2fa3922223af5560fccac921f59
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-04-02 17:57:06 +0900

    kyb

---
+++ week11/16496/16496_김영빈.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int,input().split()))
+greedy = []
+for i in nums:
+    s = str(i)
+    leng = len(s)
+    while len(s) < 10:
+        s += s[len(s)-leng]
+    greedy.append(([*list(s)],str(i)))
+greedy.sort(key = lambda x : x[0], reverse = True)
+
+ans = ""
+for i in greedy:
+    ans += i[-1]
+print(ans if int(ans) != 0 else 0)
