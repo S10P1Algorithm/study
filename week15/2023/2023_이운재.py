2e2885e0a82826bdf20c41ab6486246f898fa579
이운재 <hil00077@khu.ac.kr>
2024-04-29 22:18:36 +0900

    oon 2023,60062

--- week15/2023/2023_이운재.py
+++ week15/2023/2023_이운재.py
@@ -1,1 +1,26 @@
-pass
+import sys
+input = sys.stdin.readline
+
+def is_prime(num):
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num/2)+1):
+        if num % i == 0:
+            return False
+    return True
+
+def dfs(num):
+    if len(str(num)) == n:
+        print(num)
+        return
+    else:
+        for i in range(1, 10):
+            if is_prime(num*10+i):
+                dfs(num*10+i)
+
+n = int(input())
+
+dfs(2)
+dfs(3)
+dfs(5)
+dfs(7)
