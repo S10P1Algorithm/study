5547537c860b327ced480a3f0c0360824885cf28
이운재 <hil00077@khu.ac.kr>
2024-05-07 17:55:19 +0900

    oon 27652

--- week16/27652/27652_이운재.py
+++ week16/27652/27652_이운재.py
@@ -1,1 +1,32 @@
-pass
+N = int(input())
+
+A = []
+B = []
+res = 0
+for _ in range(N):
+    a,*b = input().split()
+    if a == 'add':
+        if b[0] == 'A':
+            A.append(b[1])
+        else:
+            B.append(b[1])
+    elif a == 'delete':
+        if b[0] == 'A':
+            A.remove(b[1])
+        else:
+            B.remove(b[1])
+    else:
+        tmp = []
+        for i in A:
+            for j in B:
+                if len(i+j) < len(b[0]):
+                    continue
+                else:
+                    for k in range(1,len(b[0])):
+
+                        if i[:k] + j[-(len(b[0])-k):] == b[0] and (i,j,i[:k],j[-(len(b[0])-k):]) not in tmp:
+                            res +=1
+                            tmp.append((i,j,i[:k],j[-(len(b[0])-k):]))
+        print(res)
+        res = 0
+
