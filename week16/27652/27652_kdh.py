4708d92debcbc1bcf7cace4cac1127ede599ff3a
Kdh <dohoon122@gmail.com>
2024-05-07 18:07:19 +0900

    week16 kdh

--- week16/27652/27652_kdh.py
+++ week16/27652/27652_kdh.py
@@ -18,6 +18,45 @@ class Trie(object):
             node = node.children[char]
         node.data = string
         return node
+    def starts_with(self, prefix):
+        current_node = self.head
+        words = []
+
+        for p in prefix:
+            if p in current_node.children:
+                current_node = current_node.children[p]
+            else:
+                return None
+
+        current_node = [current_node]
+        next_node = []
+        while True:
+            for node in current_node:
+                if node.data:
+                    words.append(node.data)
+                next_node.extend(list(node.children.values()))
+            if len(next_node) != 0:
+                current_node = next_node
+                next_node = []
+            else:
+                break
+        
+        return len(words)
+    
+    def delete(self, string):
+        node = self.head
+        stack = []
+        for char in string:
+            if char in node.children:
+                node = node.children[char]
+                stack.append(node)
+            else:
+                return None
+        while stack:
+            node =stack.pop()
+            if len(node.children) == 0:
+                node.data = None
+        return node
 
 Q = int(input())
 A = Trie()
@@ -31,17 +70,27 @@ for _ in range(Q):
         if destination == "A":
             A.insert(text)
         else:
-            B.insert(text)
+            new_text = ''.join(reversed(text))
+            B.insert(new_text)
 
     elif command == "delete":
         destination = input_list[0]
         text = input_list[1]
         if destination == "A":
-            A.insert(text)
+            A.delete(text)
         else:
-            B.insert(text)
+            new_text = ''.join(reversed(text))
+            B.delete(new_text)
             
     elif command == "find":
-        pass
+        text = input_list[0]
+        N = len(text)
+        ans = 0
+        for i in range(1, N):
+            prefix = text[:i]
+            suffix = ''.join(reversed(text[i:]))
+            if A.starts_with(prefix) and B.starts_with(suffix):
+                ans += A.starts_with(prefix) * B.starts_with(suffix)
+        print(ans)
     else:
         pass
